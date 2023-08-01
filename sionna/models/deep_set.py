3f0ffb3abc1fecddf567e60aa2bbede2b74b471d
Nicola Di Cicco <93935338+nicoladicicco@users.noreply.github.com>
2023-08-01 21:11:28 +0200

    fix class constructor

--- sionna/models/deep_set.py
+++ sionna/models/deep_set.py
@@ -18,10 +18,9 @@ class EquivariantLayer(nn.Module):
 
 
 class EquivariantDeepSet(nn.Module):
-    def __init__(self, in_channels: int, net_arch: list[int], act_fn: Type[nn.Module]) -> None:
+    def __init__(self, net_arch: list[int], act_fn: Type[nn.Module]) -> None:
         super().__init__()
         self.net = nn.Sequential()
-        net_arch = [in_channels] + net_arch
         for i in range(len(net_arch) - 2):
             self.net.append(EquivariantLayer(net_arch[i], net_arch[i + 1]))
             self.net.append(act_fn())
@@ -34,15 +33,15 @@ class EquivariantDeepSet(nn.Module):
 
 
 class InvariantDeepSet(nn.Module):
-    def __init__(self, in_channels: int, psi_arch: list[int], rho_arch: list[int], act_fn: Type[nn.Module]) -> None:
+    def __init__(self, psi_arch: list[int], rho_arch: list[int], act_fn: Type[nn.Module]) -> None:
         super().__init__()
-        self.psi = EquivariantDeepSet(in_channels, psi_arch, act_fn)
+        self.psi = EquivariantDeepSet(psi_arch, act_fn)
         rho_arch = [psi_arch[-1]] + rho_arch
         self.rho = nn.Sequential()
-        for i in range(len(rho_arch) - 1):
+        for i in range(len(rho_arch) - 2):
             self.rho.append(nn.Linear(rho_arch[i], rho_arch[i + 1]))
             self.rho.append(act_fn())
-        self.rho.append(nn.Linear(rho_arch[-1], 1))
+        self.rho.append(nn.Linear(rho_arch[-2], rho_arch[-1]))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # x: (batch_size, n_elements, in_channels)
